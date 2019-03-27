8787b54a11f6f2393a968c32f3a2db378d7e0ccf
YOB <yigitozanberk@gmail.com>
2019-03-27 23:02:14 +0200

    Part1 and part2 nailed! Part3 waiting

--- R_PROG_ASG_1.R
+++ R_PROG_ASG_1.R
@@ -3,11 +3,37 @@
 #pollutantmean
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-        mean_vector <- numeric(length = length(id))
-        file_index <- sprintf("%0.3d", 1:id)
+        mean_vector <- numeric()
+        file_index <- sprintf("%0.3d", id)
+        my_data <- list()
+        obs_vector <- numeric()
+        weight_vector <- numeric()
         
-       for (i in id) {
-         file_dir <- paste(directory, file_index[i], sep = "/")
-         my_data[i] <- read.csv()
+       for (i in seq_along(file_index)) {
+            file_dir <- paste(directory, file_index[i], sep = "/")
+            my_data <- read.csv(paste(file_dir, ".csv", sep = ""))
+            mean_vector[i] <- mean(my_data[[pollutant]], na.rm= TRUE)
+            obs_vector[i] <- nrow(my_data[!is.na(my_data[[pollutant]]), ])
+            weight_vector[i] <- mean_vector[i] * obs_vector[i]
        }
+        sum(weight_vector)/sum(obs_vector)
 }
+
+complete <- function(directory, id = 1:332) {
+        file_index <- sprintf("%0.3d", id)
+        my_data <- list()
+        obs_vector <- numeric()
+        
+        for(i in seq_along(file_index)){
+                file_dir<- paste(directory, file_index[i], sep = "/")
+                my_data <- read.csv(paste(file_dir, ".csv", sep = ""))
+                obs_vector[i]<- nrow(my_data[complete.cases(my_data),])
+        }
+        what_is_matrix <- cbind(id, obs_vector)
+        colnames(what_is_matrix) <- c("id", "nobs")
+        what_is_matrix
+}
+
+
+
+
