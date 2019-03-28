a7fb80b84384d6eba395a29379a8398d44b66241
YOB <yigitozanberk@gmail.com>
2019-03-28 22:52:16 +0200

    fine tuned the quiz codes.

--- R_PROG_ASG_1.R
+++ R_PROG_ASG_1.R
@@ -3,37 +3,68 @@
 #pollutantmean
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-        mean_vector <- numeric()
-        file_index <- sprintf("%0.3d", id)
-        my_data <- list()
-        obs_vector <- numeric()
-        weight_vector <- numeric()
+        mean_vector <- numeric() #vector to store means
+        file_index <- sprintf("%0.3d", id) #read.csv directory modifier
+        my_data <- list() #vector of list for temporary data storage
+        obs_vector <- numeric() #vector for num of observations
+        weight_vector <- numeric() #vector for weighted mean calculation
         
        for (i in seq_along(file_index)) {
-            file_dir <- paste(directory, file_index[i], sep = "/")
-            my_data <- read.csv(paste(file_dir, ".csv", sep = ""))
-            mean_vector[i] <- mean(my_data[[pollutant]], na.rm= TRUE)
-            obs_vector[i] <- nrow(my_data[!is.na(my_data[[pollutant]]), ])
-            weight_vector[i] <- mean_vector[i] * obs_vector[i]
+            file_dir <- paste(directory, file_index[i], sep = "/") #dir. init.
+            my_data <- read.csv(paste(file_dir, ".csv", sep = "")) #read data
+            mean_vector[i] <- mean(my_data[[pollutant]], na.rm= TRUE) #means
+            obs_vector[i] <- nrow(my_data[!is.na(my_data[[pollutant]]), ]) #obs
+            weight_vector[i] <- mean_vector[i] * obs_vector[i] #weighted means
+            if(is.nan(weight_vector[i]) | is.nan(obs_vector[i]) | is.nan(mean_vector[i])){
+              weight_vector[i] <- 0
+              obs_vector[i] <- 0
+              mean_vector[i] <- 0
+            }
        }
-        sum(weight_vector)/sum(obs_vector)
+        sum(weight_vector)/sum(obs_vector) #weighted mean of every file
+
 }
 
 complete <- function(directory, id = 1:332) {
-        file_index <- sprintf("%0.3d", id)
-        my_data <- list()
-        obs_vector <- numeric()
+        file_index <- sprintf("%0.3d", id) #dir. init.
+        my_data <- list() #temporary data list
+        obs_vector <- numeric() #obs vector
         
         for(i in seq_along(file_index)){
-                file_dir<- paste(directory, file_index[i], sep = "/")
-                my_data <- read.csv(paste(file_dir, ".csv", sep = ""))
-                obs_vector[i]<- nrow(my_data[complete.cases(my_data),])
+                file_dir<- paste(directory, file_index[i], sep = "/") #dir. init
+                my_data <- read.csv(paste(file_dir, ".csv", sep = "")) #read
+                obs_vector[i]<- nrow(my_data[complete.cases(my_data),]) #obs 
         }
-        what_is_matrix <- cbind(id, obs_vector)
-        colnames(what_is_matrix) <- c("id", "nobs")
-        what_is_matrix
+        what_is_matrix <- cbind(id, obs_vector) #matrix init
+        colnames(what_is_matrix) <- c("id", "nobs") #colnames init
+        what_is_matrix #return value
 }
 
 
+
+
+corr<- function(directory, threshold = 0) {
+        my_data <- list() #temporary data list
+        obs_vector <- numeric() #obs vector
+        cor_vector <- numeric() #correlation vector
+        file_index <- paste(directory, list.files(directory), sep="/")
+        count <- 1
+            for(i in seq_along(file_index)){
+                  my_data <- read.csv(file_index[i])
+                  l_data <- complete.cases(my_data)
+                  cl_data <- my_data[complete.cases(my_data), ]
+                  if(sum(l_data) > threshold){
+                        cor_vector[count] <- cor(cl_data[["sulfate"]], cl_data[["nitrate"]])
+                        count <- count + 1
+                  }
+                  
+            }
+            if(length(cor_vector) > 0){
+              return(cor_vector)
+            } else {
+              x <- numeric()
+              return(x)
+            }
+}
 
 
